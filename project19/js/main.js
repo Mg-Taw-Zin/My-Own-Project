f99977cb62dd546bfb1e6b705bb744830b8374c0
Mg-Taw-Zin <kyawkyaw91a@gmail.com>
2023-05-30 21:39:31 +0630

    5/30/2023

--- project19/js/main.js
+++ project19/js/main.js
@@ -1,47 +1,26 @@
-const apikey = "16f503380e1376d6b91062dd1a7d5ffc";
-const weatherData = document.querySelector("#weather-data");
-const cityInput = document.querySelector("#city-input");
-const form = document.querySelector("form");
+const button = document.querySelectorAll("button");
+const input = document.querySelector("#result");
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const cityValue = cityInput.value;
-  getWeatherData(cityValue);
-});
-
-async function getWeatherData(cityValue) {
-  try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
-    );
-
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener("click", () => {
+    const buttonValue = button[i].textContent;
+    console.log(buttonValue);
+    if (buttonValue === "C") {
+      clearResult();
+    } else if (buttonValue === "=") {
+      calculateResult();
+    } else {
+      appendValue(buttonValue);
     }
+  });
+}
 
-    const data = await response.json();
-    const temperature = Math.round(data.main.temp);
-    const description = data.weather[0].description;
-    const icon = data.weather[0].icon;
-    const details = [
-      `Feels like: ${Math.round(data.main.feels_like)}`,
-      `Humidity: ${data.main.humidity}%`,
-      `Wind speed: ${data.wind.speed} mph`,
-    ];
-
-    weatherData.querySelector(".icon").innerHTML = `<img
-    src="http://openweathermap.org/img/wn/${icon}.png"
-    alt="Weather Icon"/>`;
-    weatherData.querySelector(".temperature").textContent = `${temperature}°C`;
-    weatherData.querySelector(".description").textContent = description;
-    weatherData.querySelector(".details").innerHTML = details
-      .map((detail) => `<div>${detail}</div>`)
-      .join("");
-  } catch (error) {
-    weatherData.querySelector(".icon").innerHTML = "";
-    weatherData.querySelector(".temperature").textContent = "";
-    weatherData.querySelector(".description").textContent =
-      "Error 404 not found";
-    weatherData.querySelector(".details").innerHTML = "";
-  }
+function clearResult() {
+  input.value = "";
+}
+function calculateResult() {
+  input.value = eval(input.value);
+}
+function appendValue(buttonValue) {
+  input.value += buttonValue;
 }
